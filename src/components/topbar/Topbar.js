47cf775af4d45597fbd6d2a710278027a3f82a7a
taniavaldivia <tania.valdiviag@gmail.com>
2022-07-24 04:28:05 -0500

    initial commit

---
+++ src/components/topbar/Topbar.js
@@ -0,0 +1,36 @@
+import "./topbar.scss"
+import { Person, Mail } from "@mui/icons-material"
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            Tania Valdivia
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+52 8115018214</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>tuweb@tania.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div
+            className="hamburger"
+            onClick={() => {
+              setMenuOpen(!menuOpen)
+            }}
+          >
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
